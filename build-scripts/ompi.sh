83cac8b61fd002088c8087f4c43744a3918cca55
Niflheimr <jim90247@gmail.com>
2019-07-31 15:47:08 +0800

    Update openmpi build script

--- build-scripts/ompi.sh
+++ build-scripts/ompi.sh
@@ -8,6 +8,10 @@ PREFIX=/opt/pkg/bin/ompi-3.1.4/gcc-4.8.5
 
 mkdir build
 cd build
-CC=${cc} CXX=${cxx} FC=${f} CFLAGS=${opt_flag} CXXFLAGS=${opt_flag} FCFLAGS=${opt_flag} FFLAGS=${opt_flag} ../${CONFIG} --prefix=${PREFIX} --enable-orterun-prefix-by-default --enable-mpi-cxx --with-platform=../contrib/platform/mellanox/optimized
+# for cuda support, add --with-cuda=<path_to_cuda> --with-ucx=<path_to_ucx_cuda>
+CC=${cc} CXX=${cxx} FC=${f} \
+  CFLAGS=${opt_flag} CXXFLAGS=${opt_flag} FCFLAGS=${opt_flag} \
+  ../${CONFIG} --prefix=${PREFIX} --enable-orterun-prefix-by-default \
+  --enable-mpi-cxx --with-platform=../contrib/platform/mellanox/optimized
 make all -j
 make install
